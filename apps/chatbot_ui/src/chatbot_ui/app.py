1f7bb9d5db49702acde12c6b7405548f3776dc81
gjsh96 <164214917+gjsh96@users.noreply.github.com>
2026-06-29 01:52:45 -0500

    Moving the RAG Pipeline to the Backend

--- apps/chatbot_ui/src/chatbot_ui/app.py
+++ apps/chatbot_ui/src/chatbot_ui/app.py
@@ -34,22 +34,6 @@ def api_call(method, url, **kwargs):
         _show_error_popup(f"An unexpected error occurred: {str(e)}")
         return False, {"message": str(e)}
 
-#Sidebar with dropdown for the model and provider
-with st.sidebar:
-    st.title("Settings")
-
-    #Dropdown for model
-    provider = st.selectbox("PROVIDER", ["OpenAI", "Groq", "Google"])
-    if provider == "OpenAI":
-        model_name =  st.selectbox("MODEL", ["gpt-5-nano", "gpt-5-mini"])
-    elif provider == "Groq":
-        model_name =  st.selectbox("MODEL", ["llama-3.3-70b-versatile"])
-    else: 
-         model_name =  st.selectbox("MODEL", ["gemini-2.5-flash"])
-
-    #Save provider and model to session state
-    st.session_state.provider = provider
-    st.session_state.model_name =  model_name
 
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Hello! how can I assist you today?"}]
@@ -65,9 +49,9 @@ if prompt := st.chat_input("Hello! how can I help you today?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        output = api_call("post", f"{config.API_URL}/chat", json={"provider": st.session_state.provider, "models_name": st.session_state.model_name, "messages": st.session_state.messages})
+        output = api_call("post", f"{config.API_URL}/rag", json={"query": prompt})
         
         response_data = output[1]
-        answer = response_data["message"]
+        answer = response_data["answer"]
         st.write(answer)
         st.session_state.messages.append({"role": "assistant", "content": answer})
