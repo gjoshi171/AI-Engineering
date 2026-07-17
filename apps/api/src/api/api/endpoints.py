d9ff0e48f73e3c01778bc4b752492844eb95cc13
gjsh96 <164214917+gjsh96@users.noreply.github.com>
2026-07-16 22:44:08 -0500

    ReAct based AI system to backend

--- apps/api/src/api/api/endpoints.py
+++ apps/api/src/api/api/endpoints.py
@@ -1,24 +1,25 @@
 from fastapi import APIRouter, Request
-from api.api.model import RAGRequest, RAGResponse, RAGUsedContext
-from api.agents.retrieval_generation import rag_pipeline_wraper
-
+from api.api.models import RAGRequest, RAGResponse, RAGUsedContext
+from api.agents.graph import agent_wrapper
 import logging
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger(__name__)   
+logger = logging.getLogger(__name__)
 
 rag_router = APIRouter()
 
 @rag_router.post("/")
 def chat(request: Request, payload: RAGRequest) -> RAGResponse:
-    result = rag_pipeline_wraper(payload.query)
+
+    result = agent_wrapper(payload.query)
+
     return RAGResponse(
-        answer=result["answer"],
-        used_context= [RAGUsedContext(**item) for item in result["used_context"]]
-        )
+        answer=result["answer"], 
+        used_context=[RAGUsedContext(**item) for item in result["used_context"]]
+    )
 
 api_router = APIRouter()
-api_router.include_router(rag_router, prefix="/rag", tags=["rag"])
+api_router.include_router(rag_router, prefix="/agent", tags=["agent"])
